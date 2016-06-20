ac1194e53fd077783056edcd8fbf399934e9bb5e
Tuomo Untinen <tuomo.untinen@iki.fi>
2016-06-20 23:08:34 +0300

    Added defense tech.

--- src/org/openRealmOfStars/player/tech/TechFactory.java
+++ src/org/openRealmOfStars/player/tech/TechFactory.java
@@ -45,7 +45,28 @@ public class TechFactory {
   public static final String[] COMBAT_TECH_LEVEL10_NAMES = 
     {"Phasors Mk5","Massdrive Mk5","Photon Torpedo Mk10","ECM Torpedo Mk8","HE Missile Mk8"};
 
-  
+
+  public static final String[] DEFENSE_TECH_LEVEL1_NAMES = 
+    {"Shield Mk1","Armor Plating Mk1"};
+  public static final String[] DEFENSE_TECH_LEVEL2_NAMES = 
+    {"Shield Mk2","Armor Plating Mk2","Planetary Defense Turret Mk1"};
+  public static final String[] DEFENSE_TECH_LEVEL3_NAMES = 
+    {"Shield Mk3","Armor Plating Mk3"};
+  public static final String[] DEFENSE_TECH_LEVEL4_NAMES = 
+    {"Shield Mk4","Armor Plating Mk4","Shield Generator Mk1"};
+  public static final String[] DEFENSE_TECH_LEVEL5_NAMES = 
+    {"Shield Mk5","Armor Plating Mk5","Planetary Defense Turret Mk2"};
+  public static final String[] DEFENSE_TECH_LEVEL6_NAMES = 
+    {"Shield Mk6","Armor Plating Mk6"};
+  public static final String[] DEFENSE_TECH_LEVEL7_NAMES = 
+    {"Shield Mk7","Armor Plating Mk7"};
+  public static final String[] DEFENSE_TECH_LEVEL8_NAMES = 
+    {"Shield Mk8","Armor Plating Mk8"};
+  public static final String[] DEFENSE_TECH_LEVEL9_NAMES = 
+    {"Shield Mk9","Armor Plating Mk9","Shield Generator Mk2"};
+  public static final String[] DEFENSE_TECH_LEVEL10_NAMES = 
+    {"Shield Mk10","Armor Plating Mk10"};
+
   /**
    * Create combat tech with certain name and level
    * @param name Tech Name
@@ -77,5 +98,41 @@ public class TechFactory {
     }
     return null;
   }
-  
+
+  /**
+   * Create defense tech with certain name and level
+   * @param name Tech Name
+   * @param level level between 1-10
+   * @return Tech or null if match not found
+   */
+  public static Tech createDefenseTech(String name, int level) {
+    String[] list;
+    switch (level) {
+    case 1: list = DEFENSE_TECH_LEVEL1_NAMES; break;
+    case 2: list = DEFENSE_TECH_LEVEL2_NAMES; break;
+    case 3: list = DEFENSE_TECH_LEVEL3_NAMES; break;
+    case 4: list = DEFENSE_TECH_LEVEL4_NAMES; break;
+    case 5: list = DEFENSE_TECH_LEVEL5_NAMES; break;
+    case 6: list = DEFENSE_TECH_LEVEL6_NAMES; break;
+    case 7: list = DEFENSE_TECH_LEVEL7_NAMES; break;
+    case 8: list = DEFENSE_TECH_LEVEL8_NAMES; break;
+    case 9: list = DEFENSE_TECH_LEVEL9_NAMES; break;
+    case 10: list = DEFENSE_TECH_LEVEL10_NAMES; break;
+    default: return null;
+    }
+    for (int i=0;i<list.length;i++) {
+      String techName = list[i];
+      if (name.equals(techName)) {
+        Tech tech = new Tech(techName, TechType.Combat, level);
+        if (techName.startsWith("Planetary Defense Turret Mk")) {
+          tech.setImprovement(techName);
+        } else {
+          tech.setComponent(techName);
+        }
+        return tech;
+      }
+    }
+    return null;
+  }
+
 }

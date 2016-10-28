827da09ea4f531a6c9779c639fd8e78f1f0da6e6
Tuomo Untinen <tuomo.untinen@iki.fi>
2016-10-29 00:07:08 +0300

    Added Copyright and fixed the second test.

--- src/test/java/org/openRealmOfStars/starMap/SunTest.java
+++ src/test/java/org/openRealmOfStars/starMap/SunTest.java
@@ -1,17 +1,37 @@
 package org.openRealmOfStars.starMap;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-
+/**
+ * 
+ * Open Realm of Stars game project
+ * Copyright (C) 2016  Tuomo Untinen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses/
+ * 
+ * 
+ * Test for Sun class
+ * 
+ */
 public class SunTest {
 
-    private Sun sun;
 
     @Test
     public void testCreateSunWithName(){
-        sun = new Sun(10, 15, "Sun name");
+        Sun sun = new Sun(10, 15, "Sun name");
 
         assertEquals("Sun name", sun.getName());
         assertEquals(10, sun.getCenterX());
@@ -19,11 +39,11 @@ public class SunTest {
     }
 
     @Test
-    @Ignore(value = "static RandomSystemNameGenerator.generate() should be mockable.")
     public void testCreateSunWhereNameIsNull(){
-        sun = new Sun(10, 15, null);
+        Sun sun = new Sun(10, 15, null);
 
-        assertEquals("Sun name", sun.getName());
+        assertNotNull(sun.getName());
+        assertEquals(true, sun.getName().contains(" "));
         assertEquals(10, sun.getCenterX());
         assertEquals(15, sun.getCenterY());
     }

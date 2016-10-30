7d67d3742b527f363d2702df36ce9867b2042386
sigee <sigee15@gmail.com>
2016-10-30 16:37:04 +0100

    Add JavaDoc And Copyright

--- src/main/java/org/openRealmOfStars/utilities/Logger.java
+++ src/main/java/org/openRealmOfStars/utilities/Logger.java
@@ -1,5 +1,26 @@
 package org.openRealmOfStars.utilities;
-
+/**
+ *
+ * Open Realm of Stars game project
+ * Copyright (C) 2016  Tuomo Untinen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses/
+ *
+ *
+ * Logger class
+ *
+ */
 public class Logger {
 
     private static final int DEFAULT_LOG_SIZE = 11;
@@ -17,6 +38,10 @@ public class Logger {
         }
     }
 
+    /**
+     * Add new text log
+     * @param message Text to add
+     */
     public void addLog(String message) {
         System.arraycopy(textLog, 0, textLog, 1, textLog.length - 1);
         textLog[0] = message;
--- src/test/java/org/openRealmOfStars/utilities/LoggerTest.java
+++ src/test/java/org/openRealmOfStars/utilities/LoggerTest.java
@@ -3,7 +3,28 @@ package org.openRealmOfStars.utilities;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+/**
+ *
+ * Open Realm of Stars game project
+ * Copyright (C) 2016  Tuomo Untinen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses/
+ *
+ *
+ * Test for Logger class
+ *
+ */
 public class LoggerTest {
 
     private static final int LOG_SIZE = 5;

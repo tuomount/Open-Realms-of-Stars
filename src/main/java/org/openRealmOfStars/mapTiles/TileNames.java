fc4bc807538734acf3c3365de797814ed6565863
Tuomo Untinen <tuomo.untinen@iki.fi>
2016-11-18 06:53:39 +0200

    Fixed checkstyle errors.

--- src/main/java/org/openRealmOfStars/mapTiles/TileNames.java
+++ src/main/java/org/openRealmOfStars/mapTiles/TileNames.java
@@ -23,55 +23,151 @@ package org.openRealmOfStars.mapTiles;
  *
  */
 
-public class TileNames {
+public final class TileNames {
 
+  /**
+   * Hiding the constructor
+   */
+  private TileNames() {
+    //Nothing to do
+  }
   /**
    * Name for empty tile which is not drawn
    */
   public static final String EMPTY = "EMPTY";
 
-  // Names for Sun tiles
+  /**
+   * Sun North West
+   */
   public static final String SUN_NW = "SUN_NW";
+  /**
+   * Sun North
+   */
   public static final String SUN_N = "SUN_N";
+  /**
+   * Sun North East
+   */
   public static final String SUN_NE = "SUN_NE";
+  /**
+   * Sun East
+   */
   public static final String SUN_E = "SUN_E";
+  /**
+   * Sun South East
+   */
   public static final String SUN_SE = "SUN_SE";
+  /**
+   * Sun South
+   */
   public static final String SUN_S = "SUN_S";
+  /**
+   * Sun South West
+   */
   public static final String SUN_SW = "SUN_SW";
+  /**
+   * Sun West
+   */
   public static final String SUN_W = "SUN_W";
+  /**
+   * Sun Center
+   */
   public static final String SUN_C = "SUN_C";
 
-  // Names for Gas Giant 1
+  /**
+   * Gas Giant 1 North West
+   */
   public static final String GAS_GIANT_1_NW = "GAS_GIANT_1_NW";
+  /**
+   * Gas Giant 1 North East
+   */
   public static final String GAS_GIANT_1_NE = "GAS_GIANT_1_NE";
+  /**
+   * Gas Giant 1 South East
+   */
   public static final String GAS_GIANT_1_SE = "GAS_GIANT_1_SE";
+  /**
+   * Gas Giant 1 South West
+   */
   public static final String GAS_GIANT_1_SW = "GAS_GIANT_1_SW";
 
-  // Names for Gas Giant 2
+  /**
+   * Gas Giant 2 North West
+   */
   public static final String GAS_GIANT_2_NW = "GAS_GIANT_2_NW";
+  /**
+   * Gas Giant 2 North East
+   */
   public static final String GAS_GIANT_2_NE = "GAS_GIANT_2_NE";
+  /**
+   * Gas Giant 2 South East
+   */
   public static final String GAS_GIANT_2_SE = "GAS_GIANT_2_SE";
+  /**
+   * Gas Giant 2 South West
+   */
   public static final String GAS_GIANT_2_SW = "GAS_GIANT_2_SW";
 
-  // Name for Planets
+  /**
+   * Planet 1 Rock world 1
+   */
   public static final String ROCK1 = "ROCK1";
+  /**
+   * Planet 2 Water world 1
+   */
   public static final String WATERWORLD1 = "WATERWORLD1";
+  /**
+   * Planet 3 Water world 2
+   */
   public static final String WATERWORLD2 = "WATERWORLD2";
+  /**
+   * Planet 4 Iron world 1
+   */
   public static final String IRONPLANET1 = "IRONPLANET1";
+  /**
+   * Planet 5 Iron world 2
+   */
   public static final String IRONPLANET2 = "IRONPLANET2";
 
-  // Fog of war and uncharted
+  /**
+   * Fog of war tile (Overlay)
+   */
   public static final String FOG_OF_WAR = "FOG_OF_WAR";
+  /**
+   * Uncharted tile (Overlay)
+   */
   public static final String UNCHARTED = "UNCHARTED";
 
-  // Culture tiles
+  /**
+   * Culture tile for player 0 (Overlay)
+   */
   public static final String PLAYER_0 = "Player_0";
+  /**
+   * Culture tile for player 1 (Overlay)
+   */
   public static final String PLAYER_1 = "Player_1";
+  /**
+   * Culture tile for player 2 (Overlay)
+   */
   public static final String PLAYER_2 = "Player_2";
+  /**
+   * Culture tile for player 3 (Overlay)
+   */
   public static final String PLAYER_3 = "Player_3";
+  /**
+   * Culture tile for player 4 (Overlay)
+   */
   public static final String PLAYER_4 = "Player_4";
+  /**
+   * Culture tile for player 5 (Overlay)
+   */
   public static final String PLAYER_5 = "Player_5";
+  /**
+   * Culture tile for player 6 (Overlay)
+   */
   public static final String PLAYER_6 = "Player_6";
+  /**
+   * Culture tile for player 7 (Overlay)
+   */
   public static final String PLAYER_7 = "Player_7";
 
 }

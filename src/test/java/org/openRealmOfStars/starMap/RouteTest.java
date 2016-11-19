ab0de15059cd8508e2e42701c4e1984945872009
sigee <sigee15@gmail.com>
2016-11-19 10:25:42 +0100

    Split up the mixed test method to three independent test cases + remove TODO (There aren't dependencies).

--- src/test/java/org/openRealmOfStars/starMap/RouteTest.java
+++ src/test/java/org/openRealmOfStars/starMap/RouteTest.java
@@ -23,7 +23,7 @@ import org.junit.Test;
  * 
  * 
  * Test for Route class
- * @TODO: Mock dependencies
+ *
  */
 public class RouteTest {
 
@@ -34,103 +34,110 @@ public class RouteTest {
     int step = 0;
     while (!route.isEndReached()) {
       if (step == 0) {
-        assertEquals(3,route.getX());
-        assertEquals(3,route.getY());
+        assertEquals(3, route.getX());
+        assertEquals(3, route.getY());
       }
       if (step == 1) {
-        assertEquals(4,route.getX());
-        assertEquals(4,route.getY());
+        assertEquals(4, route.getX());
+        assertEquals(4, route.getY());
       }
       step++;
       route.makeNextMove();
     }
     if (step == 2) {
-      assertEquals(5,route.getX());
-      assertEquals(5,route.getY());
+      assertEquals(5, route.getX());
+      assertEquals(5, route.getY());
     }
-    route = new Route(2, 2, 8, 8, 2);
+  }
+
+  @Test
+  public void testRoute2() {
+    Route route = new Route(2, 2, 8, 8, 2);
     assertEquals(3, route.timeEstimate());
-    step = 0;
+    int step = 0;
     while (!route.isEndReached()) {
       if (step == 0) {
-        assertEquals(2,route.getX());
-        assertEquals(2,route.getY());
+        assertEquals(2, route.getX());
+        assertEquals(2, route.getY());
       }
       if (step == 1) {
-        assertEquals(4,route.getX());
-        assertEquals(4,route.getY());
+        assertEquals(4, route.getX());
+        assertEquals(4, route.getY());
       }
       if (step == 2) {
-        assertEquals(6,route.getX());
-        assertEquals(6,route.getY());
+        assertEquals(6, route.getX());
+        assertEquals(6, route.getY());
       }
       step++;
       route.makeNextMove();
     }
     if (step == 3) {
-      assertEquals(8,route.getX());
-      assertEquals(8,route.getY());
+      assertEquals(8, route.getX());
+      assertEquals(8, route.getY());
     }
-    route = new Route(9, 9, 2, 5, 1);
+  }
+
+  @Test
+  public void testRoute3() {
+    Route route = new Route(9, 9, 2, 5, 1);
     assertEquals(7, route.timeEstimate());
-    step = 0;
+    int step = 0;
     while (!route.isEndReached()) {
-      System.out.println("Step:"+step+" X: "+route.getX()+" Y: "+route.getY());
       if (step == 0) {
-        assertEquals(9,route.getX());
-        assertEquals(9,route.getY());
+        assertEquals(9, route.getX());
+        assertEquals(9, route.getY());
       }
       if (step == 1) {
-        assertEquals(8,route.getX());
-        assertEquals(8,route.getY());
+        assertEquals(8, route.getX());
+        assertEquals(8, route.getY());
       }
       if (step == 2) {
-        assertEquals(7,route.getX());
-        assertEquals(8,route.getY());
+        assertEquals(7, route.getX());
+        assertEquals(8, route.getY());
       }
       if (step == 3) {
-        assertEquals(6,route.getX());
-        assertEquals(7,route.getY());
+        assertEquals(6, route.getX());
+        assertEquals(7, route.getY());
       }
       if (step == 4) {
-        assertEquals(5,route.getX());
-        assertEquals(7,route.getY());
+        assertEquals(5, route.getX());
+        assertEquals(7, route.getY());
       }
       if (step == 5) {
-        assertEquals(4,route.getX());
-        assertEquals(6,route.getY());
+        assertEquals(4, route.getX());
+        assertEquals(6, route.getY());
       }
       if (step == 6) {
-        assertEquals(3,route.getX());
-        assertEquals(6,route.getY());
+        assertEquals(3, route.getX());
+        assertEquals(6, route.getY());
       }
       step++;
       route.makeNextMove();
     }
     if (step == 7) {
-      assertEquals(2,route.getX());
-      assertEquals(5,route.getY());
+      assertEquals(2, route.getX());
+      assertEquals(5, route.getY());
     }
   }
 
   @Test
   public void testIsDefending() {
     Route route = new Route(3, 3, 5, 5, 0);
-    assertEquals(true,route.isDefending());
+    assertEquals(true, route.isDefending());
     route = new Route(3, 3, 5, 5, 1);
-    assertEquals(false,route.isDefending());
+    assertEquals(false, route.isDefending());
     route = new Route(3, 3, 5, 5, Route.ROUTE_FIX);
-    assertEquals(false,route.isDefending());
+    assertEquals(false, route.isDefending());
   }
 
   @Test
   public void testIsFixing() {
     Route route = new Route(3, 3, 5, 5, Route.ROUTE_FIX);
-    assertEquals(true,route.isFixing());
+    assertEquals(true, route.isFixing());
     route = new Route(3, 3, 5, 5, 1);
-    assertEquals(false,route.isFixing());
+    assertEquals(false, route.isFixing());
     route = new Route(3, 3, 5, 5, Route.ROUTE_DEFEND);
-    assertEquals(false,route.isFixing());
+    assertEquals(false, route.isFixing());
   }
 
 }

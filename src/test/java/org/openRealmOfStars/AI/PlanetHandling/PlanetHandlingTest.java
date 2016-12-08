58921716fdd1ac2f07c545b773b15964b2dcfc5f
Tuomo Untinen <tuomo.untinen@iki.fi>
2016-12-08 22:49:16 +0200

    Refactored JUnit.

--- src/test/java/org/openRealmOfStars/AI/PlanetHandling/PlanetHandlingTest.java
+++ src/test/java/org/openRealmOfStars/AI/PlanetHandling/PlanetHandlingTest.java
@@ -35,9 +35,11 @@ import org.openRealmOfStars.starMap.planet.construction.BuildingType;
 
 public class PlanetHandlingTest {
 
-  @Test
-  @Category(org.openRealmOfStars.UnitTest.class)
-  public void testBasicFarmBuildingScoring() {
+  /**
+   * Create basic farm with mockito
+   * @return basic farm building
+   */
+  private static Building createBasicFarm() {
     Building building = Mockito.mock(Building.class);
     Mockito.when(building.getBattleBonus()).thenReturn(0);
     Mockito.when(building.getDefenseDamage()).thenReturn(0);
@@ -55,6 +57,88 @@ public class PlanetHandlingTest {
     Mockito.when(building.getMetalCost()).thenReturn(7);
     Mockito.when(building.getProdCost()).thenReturn(14);
     Mockito.when(building.getName()).thenReturn("Basic farm");
+    return building;
+  }
+
+  /**
+   * Create basic factory with mockito
+   * @return basic factory building
+   */
+  private static Building createBasicFactory() {
+    Building building = Mockito.mock(Building.class);
+    Mockito.when(building.getBattleBonus()).thenReturn(0);
+    Mockito.when(building.getDefenseDamage()).thenReturn(0);
+    Mockito.when(building.getScanRange()).thenReturn(0);
+    Mockito.when(building.getScanCloakingDetection()).thenReturn(0);
+    Mockito.when(building.getType()).thenReturn(BuildingType.FACTORY);
+    Mockito.when(building.getFactBonus()).thenReturn(1);
+    Mockito.when(building.getMineBonus()).thenReturn(0);
+    Mockito.when(building.getFarmBonus()).thenReturn(0);
+    Mockito.when(building.getReseBonus()).thenReturn(0);
+    Mockito.when(building.getCultBonus()).thenReturn(0);
+    Mockito.when(building.getCredBonus()).thenReturn(0);
+    Mockito.when(building.getRecycleBonus()).thenReturn(0);
+    Mockito.when(building.getMaintenanceCost()).thenReturn(0.0);
+    Mockito.when(building.getMetalCost()).thenReturn(7);
+    Mockito.when(building.getProdCost()).thenReturn(14);
+    Mockito.when(building.getName()).thenReturn("Basic factory");
+    return building;
+  }
+
+  /**
+   * Create basic lab with mockito
+   * @return basic lab building
+   */
+  private static Building createBasicLab() {
+    Building building = Mockito.mock(Building.class);
+    Mockito.when(building.getBattleBonus()).thenReturn(0);
+    Mockito.when(building.getDefenseDamage()).thenReturn(0);
+    Mockito.when(building.getScanRange()).thenReturn(0);
+    Mockito.when(building.getScanCloakingDetection()).thenReturn(0);
+    Mockito.when(building.getType()).thenReturn(BuildingType.RESEARCH);
+    Mockito.when(building.getFactBonus()).thenReturn(0);
+    Mockito.when(building.getMineBonus()).thenReturn(0);
+    Mockito.when(building.getFarmBonus()).thenReturn(0);
+    Mockito.when(building.getReseBonus()).thenReturn(1);
+    Mockito.when(building.getCultBonus()).thenReturn(0);
+    Mockito.when(building.getCredBonus()).thenReturn(0);
+    Mockito.when(building.getRecycleBonus()).thenReturn(0);
+    Mockito.when(building.getMaintenanceCost()).thenReturn(0.0);
+    Mockito.when(building.getMetalCost()).thenReturn(7);
+    Mockito.when(building.getProdCost()).thenReturn(14);
+    Mockito.when(building.getName()).thenReturn("Basic lab");
+    return building;
+  }
+
+  /**
+   * Create basic mine with mockito
+   * @return basic mine building
+   */
+  private static Building createBasicMine() {
+    Building building = Mockito.mock(Building.class);
+    Mockito.when(building.getBattleBonus()).thenReturn(0);
+    Mockito.when(building.getDefenseDamage()).thenReturn(0);
+    Mockito.when(building.getScanRange()).thenReturn(0);
+    Mockito.when(building.getScanCloakingDetection()).thenReturn(0);
+    Mockito.when(building.getType()).thenReturn(BuildingType.MINE);
+    Mockito.when(building.getFactBonus()).thenReturn(0);
+    Mockito.when(building.getMineBonus()).thenReturn(1);
+    Mockito.when(building.getFarmBonus()).thenReturn(0);
+    Mockito.when(building.getReseBonus()).thenReturn(0);
+    Mockito.when(building.getCultBonus()).thenReturn(0);
+    Mockito.when(building.getCredBonus()).thenReturn(0);
+    Mockito.when(building.getRecycleBonus()).thenReturn(0);
+    Mockito.when(building.getMaintenanceCost()).thenReturn(0.0);
+    Mockito.when(building.getMetalCost()).thenReturn(7);
+    Mockito.when(building.getProdCost()).thenReturn(14);
+    Mockito.when(building.getName()).thenReturn("Basic mine");
+    return building;
+  }
+
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testBasicFarmBuildingScoring() {
+    Building building = createBasicFarm();
 
     Planet planet = Mockito.mock(Planet.class);
     Mockito.when(planet.getAmountMetalInGround()).thenReturn(5000);
@@ -71,23 +155,7 @@ public class PlanetHandlingTest {
   @Test
   @Category(org.openRealmOfStars.UnitTest.class)
   public void testBasicFactoryBuildingScoring() {
-    Building building = Mockito.mock(Building.class);
-    Mockito.when(building.getBattleBonus()).thenReturn(0);
-    Mockito.when(building.getDefenseDamage()).thenReturn(0);
-    Mockito.when(building.getScanRange()).thenReturn(0);
-    Mockito.when(building.getScanCloakingDetection()).thenReturn(0);
-    Mockito.when(building.getType()).thenReturn(BuildingType.FARM);
-    Mockito.when(building.getFactBonus()).thenReturn(1);
-    Mockito.when(building.getMineBonus()).thenReturn(0);
-    Mockito.when(building.getFarmBonus()).thenReturn(0);
-    Mockito.when(building.getReseBonus()).thenReturn(0);
-    Mockito.when(building.getCultBonus()).thenReturn(0);
-    Mockito.when(building.getCredBonus()).thenReturn(0);
-    Mockito.when(building.getRecycleBonus()).thenReturn(0);
-    Mockito.when(building.getMaintenanceCost()).thenReturn(0.0);
-    Mockito.when(building.getMetalCost()).thenReturn(7);
-    Mockito.when(building.getProdCost()).thenReturn(14);
-    Mockito.when(building.getName()).thenReturn("Basic factory");
+    Building building = createBasicFactory();
 
     Planet planet = Mockito.mock(Planet.class);
     Mockito.when(planet.getAmountMetalInGround()).thenReturn(5000);
@@ -104,23 +172,7 @@ public class PlanetHandlingTest {
   @Test
   @Category(org.openRealmOfStars.UnitTest.class)
   public void testBasicLabBuildingScoring() {
-    Building building = Mockito.mock(Building.class);
-    Mockito.when(building.getBattleBonus()).thenReturn(0);
-    Mockito.when(building.getDefenseDamage()).thenReturn(0);
-    Mockito.when(building.getScanRange()).thenReturn(0);
-    Mockito.when(building.getScanCloakingDetection()).thenReturn(0);
-    Mockito.when(building.getType()).thenReturn(BuildingType.FARM);
-    Mockito.when(building.getFactBonus()).thenReturn(0);
-    Mockito.when(building.getMineBonus()).thenReturn(0);
-    Mockito.when(building.getFarmBonus()).thenReturn(0);
-    Mockito.when(building.getReseBonus()).thenReturn(1);
-    Mockito.when(building.getCultBonus()).thenReturn(0);
-    Mockito.when(building.getCredBonus()).thenReturn(0);
-    Mockito.when(building.getRecycleBonus()).thenReturn(0);
-    Mockito.when(building.getMaintenanceCost()).thenReturn(0.0);
-    Mockito.when(building.getMetalCost()).thenReturn(7);
-    Mockito.when(building.getProdCost()).thenReturn(14);
-    Mockito.when(building.getName()).thenReturn("Basic lab");
+    Building building = createBasicLab();
 
     Planet planet = Mockito.mock(Planet.class);
     Mockito.when(planet.getAmountMetalInGround()).thenReturn(5000);
@@ -137,23 +189,7 @@ public class PlanetHandlingTest {
   @Test
   @Category(org.openRealmOfStars.UnitTest.class)
   public void testBasicMineBuildingScoring() {
-    Building building = Mockito.mock(Building.class);
-    Mockito.when(building.getBattleBonus()).thenReturn(0);
-    Mockito.when(building.getDefenseDamage()).thenReturn(0);
-    Mockito.when(building.getScanRange()).thenReturn(0);
-    Mockito.when(building.getScanCloakingDetection()).thenReturn(0);
-    Mockito.when(building.getType()).thenReturn(BuildingType.FARM);
-    Mockito.when(building.getFactBonus()).thenReturn(0);
-    Mockito.when(building.getMineBonus()).thenReturn(1);
-    Mockito.when(building.getFarmBonus()).thenReturn(0);
-    Mockito.when(building.getReseBonus()).thenReturn(0);
-    Mockito.when(building.getCultBonus()).thenReturn(0);
-    Mockito.when(building.getCredBonus()).thenReturn(0);
-    Mockito.when(building.getRecycleBonus()).thenReturn(0);
-    Mockito.when(building.getMaintenanceCost()).thenReturn(0.0);
-    Mockito.when(building.getMetalCost()).thenReturn(7);
-    Mockito.when(building.getProdCost()).thenReturn(14);
-    Mockito.when(building.getName()).thenReturn("Basic mine");
+    Building building = createBasicMine();
 
     Planet planet = Mockito.mock(Planet.class);
     Mockito.when(planet.getAmountMetalInGround()).thenReturn(5000);

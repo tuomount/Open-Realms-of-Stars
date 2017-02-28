77a53762733488ed040fea3d79d6942fb4d9a506
Tuomo Untinen <tuomo.untinen@iki.fi>
2017-02-28 22:13:27 +0200

    Add GalaxyStat class.
    Data model can be directly used in StatisticPanel.

---
+++ src/main/java/org/openRealmOfStars/starMap/newsCorp/GalaxyStat.java
@@ -0,0 +1,97 @@
+package org.openRealmOfStars.starMap.newsCorp;
+
+import org.openRealmOfStars.starMap.StarMap;
+
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2017  Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* News Corporation Data Model
+*
+*/
+public class GalaxyStat {
+
+  /**
+   * Galaxy Statistics name
+   */
+  private String galaxyStatisticsName;
+
+  /**
+   * Maximum number of players
+   */
+  private int maxPlayers;
+
+  /**
+   * Actual Galaxy statistics data
+   */
+  private DataModel[] dataStat;
+
+  /**
+   * Constructor for Galaxy Data model statistics.
+   * @param numberOfPlayers Number of players in game
+   * @param statName Statistics name
+   * @throws IllegalArgumentException If numberOfPlayers less than one or
+   * over StarMap.MAX_PLAYERS
+   */
+  public GalaxyStat(final int numberOfPlayers, final String statName)
+    throws IllegalArgumentException {
+    if (numberOfPlayers < 1 || numberOfPlayers > StarMap.MAX_PLAYERS) {
+      throw new IllegalArgumentException("Invalid number of players: "
+           + numberOfPlayers);
+    }
+    galaxyStatisticsName = statName;
+    maxPlayers = numberOfPlayers;
+    dataStat = new DataModel[maxPlayers];
+    for (int i = 0; i < maxPlayers; i++) {
+      dataStat[i] = new DataModel();
+    }
+  }
+
+  /**
+   * Get the actual Galaxy stat data as double integer array.
+   * StatisticPanel can show this data directly
+   * @return double int array
+   */
+  public int[][] getGalaxyData() {
+    int[][] result = new int[maxPlayers][];
+    for (int i = 0; i < maxPlayers; i++) {
+      result[i] = dataStat[i].getData();
+    }
+    return result;
+  }
+  /**
+   * Add single data value for single player
+   * @param playerIndex Player Index which to add data.
+   * @param value Value to add
+   */
+  public void addStat(final int playerIndex, final int value) {
+    if (playerIndex > -1 && playerIndex < maxPlayers) {
+      dataStat[playerIndex].addData(value);
+    }
+  }
+
+  /**
+   * Get Galaxy Statistics Data name
+   * @return  Galaxy Statistics data name
+   */
+  public String getGalaxyStatisticsName() {
+    return galaxyStatisticsName;
+  }
+
+
+}
---
+++ src/test/java/org/openRealmOfStars/starMap/newsCorp/GalaxyStatTest.java
@@ -0,0 +1,66 @@
+package org.openRealmOfStars.starMap.newsCorp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2017  Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* News Corporation Galaxy Stat Test
+*
+*/
+public class GalaxyStatTest {
+
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testGalaxyStatValid() {
+    GalaxyStat stat = new GalaxyStat(3, "Test stat");
+    assertEquals("Test stat",stat.getGalaxyStatisticsName());
+    stat.addStat(0, 1);
+    stat.addStat(0, 2);
+    stat.addStat(0, 3);
+    stat.addStat(1, 2);
+    stat.addStat(1, 4);
+    stat.addStat(1, 8);
+    stat.addStat(2, 9);
+    stat.addStat(2, 3);
+    stat.addStat(2, 0);
+    int[][] data = stat.getGalaxyData();
+    assertEquals(1,data[0][0]);
+    assertEquals(2,data[0][1]);
+    assertEquals(3,data[0][2]);
+    assertEquals(2,data[1][0]);
+    assertEquals(4,data[1][1]);
+    assertEquals(8,data[1][2]);
+    assertEquals(9,data[2][0]);
+    assertEquals(3,data[2][1]);
+    assertEquals(0,data[2][2]);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testGalaxyStatInValid() {
+    GalaxyStat stat = new GalaxyStat(-1, "Test stat");
+    // Should never reach here
+    assertEquals("Test stat",stat.getGalaxyStatisticsName());
+  }
+
+}

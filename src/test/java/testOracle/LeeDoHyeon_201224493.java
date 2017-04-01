830d343471877bd3bc1dd60ea355b629f4b774a9
Krlucete <Krlucete@gmail.com>
2017-04-01 15:04:02 +0900

    Change testOracle/ShipTest
    1. Separate a testCase to make it more accurate
    2. Change name from ShipTest to LeeDoHyeon_201224493

---
+++ src/test/java/testOracle/LeeDoHyeon_201224493.java
@@ -0,0 +1,122 @@
+package testOracle;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.openRealmOfStars.player.SpaceRace.SpaceRace;
+import org.openRealmOfStars.player.ship.Ship;
+import org.openRealmOfStars.player.ship.ShipComponent;
+import org.openRealmOfStars.player.ship.ShipDesign;
+import org.openRealmOfStars.player.ship.ShipHull;
+import org.openRealmOfStars.player.ship.ShipImages;
+
+public class LeeDoHyeon_201224493 {
+	
+	final String SHIP_DESIGN_NAME = "shipDesignNo1";
+	final String SHIP_DESIGN_INFO = "DesignInfoNo1";
+
+	final int SHIP_HULL_SLOT = 5; 
+	final int SHIP_DESIGN_COST = 100; 
+	final int SHIP_DESIGN_METALCOST = 50; 
+	final int SHIP_DESIGN_TOTALSHIELD = 20; 
+	final int SHIP_DESIGN_TOTALARMOR = 10; 
+	final int SHIP_IMAGE_INDEX = 0; 
+	final int SHIP_COMPONENT_INDEX = 0; 
+
+	ShipDesign shipdesign = Mockito.mock(ShipDesign.class, Mockito.RETURNS_DEEP_STUBS);
+	ShipHull shiphull = Mockito.mock(ShipHull.class);
+	ShipComponent weapon = Mockito.mock(ShipComponent.class);
+	ShipComponent engine = Mockito.mock(ShipComponent.class);
+	ShipComponent energy = Mockito.mock(ShipComponent.class);
+	ShipComponent armor = Mockito.mock(ShipComponent.class);
+	ShipComponent shield = Mockito.mock(ShipComponent.class);
+    ShipComponent[] shipcomponents = new ShipComponent[]{weapon,engine,energy,armor,shield};
+
+	@Before
+	public void setUp() throws Exception {
+		Mockito.when(shipdesign.getName()).thenReturn(SHIP_DESIGN_NAME);
+		Mockito.when(shipdesign.getCost()).thenReturn(SHIP_DESIGN_COST);
+		Mockito.when(shipdesign.getMetalCost()).thenReturn(SHIP_DESIGN_METALCOST);
+		Mockito.when(shipdesign.getHull()).thenReturn(shiphull);
+		Mockito.when(shipdesign.getComponentList()).thenReturn(shipcomponents);
+		Mockito.when(shipdesign.getHull().getSlotHull()).thenReturn(SHIP_HULL_SLOT);
+		Mockito.when(shipdesign.getTotalShield()).thenReturn(SHIP_DESIGN_TOTALSHIELD);
+		Mockito.when(shipdesign.getTotalArmor()).thenReturn(SHIP_DESIGN_TOTALARMOR);
+		Mockito.when(shipdesign.getHull().getImage()).thenReturn(ShipImages.getByRace(SpaceRace.HUMAN).getShipImage(SHIP_IMAGE_INDEX));
+		Mockito.when(shipdesign.getDesignInfo()).thenReturn(SHIP_DESIGN_INFO);
+	}
+
+	/**
+	* Purpose: Make sure the constructor works correctly about the inherited arguments in Construction.class
+	* Input: ship = new Ship(final ShipDesign design) 
+	* Expected: 
+	* 			design.getName() == ship.getName()
+	*			design.getProdCost() == ship.getCost()
+	*			design.getMetalCost() == ship.getMetalCost()
+	*/		
+	@Test
+	public void TestConstructorInheritedArgu(){
+	    Ship ship = new Ship(shipdesign);
+	    
+	    assertEquals(SHIP_DESIGN_NAME, ship.getName());
+	    assertEquals(SHIP_DESIGN_COST, ship.getProdCost());
+	    assertEquals(SHIP_DESIGN_METALCOST, ship.getMetalCost());
+	    assertEquals(SHIP_DESIGN_INFO, ship.getDescription());
+	}
+	
+	/**
+	* Purpose: Make sure the constructor works correctly about the InnerClass named ShipHull in ShipDesign.class
+	* Input: ship = new Ship(final ShipDesign design) 
+	* Expected: 
+	* 			design.getHull() == ship.getHull()
+	*			design.getHull().getImage() == ship.getHull().getImage()
+	*		    design.getHull().getSlotHull() == ship.getHullPointForComponent(0)
+	*/
+	@Test
+	public void TestConstructorShipHullInDesignArgu(){
+	    Ship ship = new Ship(shipdesign);
+	    
+	    assertEquals(shiphull, ship.getHull());
+	    assertEquals(ShipImages.getByRace(SpaceRace.HUMAN).getShipImage(SHIP_IMAGE_INDEX), ship.getHull().getImage());
+	    assertEquals(SHIP_HULL_SLOT, ship.getHullPointForComponent(SHIP_COMPONENT_INDEX));
+	}
+	
+	/**
+	* Purpose: Make sure the constructor works correctly about the ShipDesign argument class
+	* Input: ship = new Ship(final ShipDesign design) 
+	* Expected: 
+	* 			design.getTotalShield() == ship.getShield()
+	*			design.getTotalArmor() == ship.getArmor()
+	*/
+	@Test
+	public void TestConstructorDesignArgu(){
+	    Ship ship = new Ship(shipdesign);
+	    assertEquals(SHIP_DESIGN_TOTALSHIELD, ship.getShield());
+	    assertEquals(SHIP_DESIGN_TOTALARMOR, ship.getArmor());
+	}
+	
+	/**
+	* Purpose: Make sure the constructor works correctly about variables to add in Constructor
+	* Input: ship = new Ship(final ShipDesign design) 
+	* Expected: 
+	* 			0 == ship.getColonist()
+	*			0 == ship.getMetal()
+	*			0 == ship.getExperience()
+	*   		0 == ship.getCulture()
+	*/
+	@Test
+	public void TestConstructorVariables(){
+	    Ship ship = new Ship(shipdesign);
+	    assertEquals(0, ship.getColonist());
+	    assertEquals(0, ship.getMetal());
+	    assertEquals(0, ship.getExperience());
+	    assertEquals(0, ship.getCulture());	
+	}
+	
+	
+}

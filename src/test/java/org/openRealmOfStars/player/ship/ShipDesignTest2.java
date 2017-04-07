409909f5f825ff7cd857ddd81e8674259a3d28d5
wksdn18 <wksdn18@gmail.com>
2017-04-07 18:46:04 +0900

    ship design test2

--- src/test/java/org/openRealmOfStars/player/ship/ShipDesignTest2.java
+++ src/test/java/org/openRealmOfStars/player/ship/ShipDesignTest2.java
@@ -1,4 +1,5 @@
 package org.openRealmOfStars.player.ship;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -11,6 +12,29 @@ import org.openRealmOfStars.player.ship.ShipDesign;
 import org.openRealmOfStars.player.ship.ShipHull;
 import org.openRealmOfStars.player.ship.ShipHullFactory;
 import org.openRealmOfStars.player.ship.ShipSize;
+
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2016  Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* Class for handling text without repainting content
+*
+*/
 
 public class ShipDesignTest2 {
 

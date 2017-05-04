a4f320913050c812a00777c1536df531caa0b282
Krlucete <Krlucete@gmail.com>
2017-05-04 22:51:13 +0900

    1. Use Extract Method 2. Method hasComponentEnergy in Ship.java 3. To reduce the length of a method body (Code Smells - Long Method)

--- src/main/java/org/openRealmOfStars/player/ship/Ship.java
+++ src/main/java/org/openRealmOfStars/player/ship/Ship.java
@@ -280,6 +280,7 @@ private boolean isIndexValid(final int index) {
     boolean generatorUp = false;
     for (int i = 0; i < components.size(); i++) {
       ShipComponent comp = components.get(i);
+
       if (comp.getType() == ShipComponentType.SHIELD && componentIsWorking(i)
           && !shieldsUp) {
         workingShields = true;
@@ -294,7 +295,6 @@ private boolean isIndexValid(final int index) {
         if (shield + comp.getDefenseValue() <= getTotalShield()) {
           shield = shield + comp.getDefenseValue();
         }
-
       }
     }
     if (!workingShields) {
@@ -309,21 +309,28 @@ private boolean isIndexValid(final int index) {
    * @return true if has energy
    */
   public boolean hasComponentEnergy(final int index) {
+      return (components.size() > 0
+              && (getRemainingEnergy(index) >= 0
+              || components.get(index).getEnergyRequirement() == 0));
+  }
+
+/**
+ * @param index Component index
+ * @return remaining energy
+ */
+private int getRemainingEnergy(final int index) {
     int energy = getTotalEnergy();
     for (int i = 0; i < components.size(); i++) {
       ShipComponent comp = components.get(i);
       if (hullPoints[i] > 0 && comp.getEnergyRequirement() > 0) {
         energy = energy - comp.getEnergyRequirement();
       }
-      if (index == i && (energy >= 0 || comp.getEnergyRequirement() == 0)) {
-        return true;
-      }
-      if (index == i && energy < 0) {
-        return false;
+      if (index == i) {
+          break;
       }
     }
-    return false;
-  }
+    return energy;
+}
 
   /**
    * Check if certain component has energy and hull points so it is functioning.

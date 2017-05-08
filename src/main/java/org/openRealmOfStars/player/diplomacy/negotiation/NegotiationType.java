7673fd46cb5b05f2be2efb0a7c05a8ec2cc2dca6
Tuomo Untinen <tuomo.untinen@iki.fi>
2017-05-08 22:38:12 +0300

    Started negotiation package for diplomacy.

---
+++ src/main/java/org/openRealmOfStars/player/diplomacy/negotiation/NegotiationType.java
@@ -0,0 +1,62 @@
+package org.openRealmOfStars.player.diplomacy.negotiation;
+
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2017  Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* Negotiation type
+*
+*/
+public enum NegotiationType {
+  /**
+   * Credit offering
+   */
+  CREDIT,
+  /**
+   * Planet offering
+   */
+  PLANET,
+  /**
+   * Fleet offering
+   */
+  FLEET,
+  /**
+   * Tech offering
+   */
+  TECH,
+  /**
+   * Peace offering
+   */
+  PEACE,
+  /**
+   * Trade alliance offering
+   */
+  TRADE_ALLIANCE,
+  /**
+   * Alliance offering
+   */
+  ALLIANCE,
+  /**
+   * Map offering
+   */
+  MAP,
+  /**
+   * Captured diplomat offering
+   */
+  DIPLOMAT
+}

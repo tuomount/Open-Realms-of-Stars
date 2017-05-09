b2655c5c0454ec3df5849e1321dcfde81056877f
dlahtl1004 <dlqjagur2@naver.com>
2017-05-09 14:01:09 +0900

    Update Ship.java

--- src/main/java/org/openRealmOfStars/player/ship/Ship.java
+++ src/main/java/org/openRealmOfStars/player/ship/Ship.java
@@ -18,6 +18,7 @@ import org.openRealmOfStars.utilities.IOUtilities;
 /**
  *
  * Open Realm of Stars game project
+ * Copyright (C) 2017 Lucas
  * Copyright (C) 2016,2017  Tuomo Untinen
  *
  * This program is free software; you can redistribute it and/or

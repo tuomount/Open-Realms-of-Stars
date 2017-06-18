6da2c68cdde778f88233410a649a9fb0b2adba90
Tuomo Untinen <tuomo.untinen@iki.fi>
2017-06-18 22:30:56 +0300

    Updates DiplomacyView.

--- src/main/java/org/openRealmOfStars/game/States/DiplomacyView.java
+++ src/main/java/org/openRealmOfStars/game/States/DiplomacyView.java
@@ -1,14 +1,24 @@
 package org.openRealmOfStars.game.States;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
 import org.openRealmOfStars.game.GameCommands;
+import org.openRealmOfStars.gui.ListRenderers.TechListRenderer;
 import org.openRealmOfStars.gui.buttons.SpaceButton;
 import org.openRealmOfStars.gui.infopanel.InfoPanel;
 import org.openRealmOfStars.gui.panels.BlackPanel;
 import org.openRealmOfStars.gui.panels.RaceImagePanel;
 import org.openRealmOfStars.player.PlayerInfo;
+import org.openRealmOfStars.player.diplomacy.DiplomaticTrade;
+import org.openRealmOfStars.player.tech.Tech;
 import org.openRealmOfStars.starMap.StarMap;
 
 /**
@@ -54,6 +64,20 @@ public class DiplomacyView extends BlackPanel {
   private StarMap starMap;
 
   /**
+   * Trade which is about to happen
+   */
+  private DiplomaticTrade trade;
+
+  /**
+   * Human tech list offerings
+   */
+  private JList<Tech> humanTechListOffer;
+  /**
+   * AI tech list offerings
+   */
+  private JList<Tech> aiTechListOffer;
+
+  /**
    * Diplomacy View constructor
    * @param info1 Human player PlayerInfo
    * @param info2 AI player PlayerInfo
@@ -62,11 +86,37 @@ public class DiplomacyView extends BlackPanel {
    */
   public DiplomacyView(final PlayerInfo info1, final PlayerInfo info2,
       final StarMap map, final ActionListener listener) {
+    this.setLayout(new BorderLayout());
     human = info1;
     ai = info2;
     starMap = map;
+    int humanIndex = starMap.getPlayerList().getIndex(human);
+    int aiIndex = starMap.getPlayerList().getIndex(ai);
+    trade = new DiplomaticTrade(starMap, humanIndex, aiIndex);
+    InfoPanel center = new InfoPanel();
+    center.setTitle("Diplomacy with " + ai.getEmpireName());
+    center.setLayout(new GridLayout(0, 3));
+    InfoPanel humanOffer = new InfoPanel();
+    humanOffer.setTitle("Your offer");
+    humanOffer.setLayout(new BoxLayout(humanOffer, BoxLayout.Y_AXIS));
+    humanTechListOffer = createTechList(trade.getTradeableTechListForFirst());
+    JScrollPane scroll = new JScrollPane(humanTechListOffer);
+    humanOffer.add(scroll);
+    center.add(humanOffer);
+
     RaceImagePanel aiImg = new RaceImagePanel();
     aiImg.setRaceToShow(ai.getRace().getNameSingle());
+    center.add(aiImg);
+
+    InfoPanel aiOffer = new InfoPanel();
+    aiOffer.setTitle(ai.getEmpireName() + " offer");
+    aiTechListOffer = createTechList(trade.getTradeableTechListForSecond());
+    scroll = new JScrollPane(aiTechListOffer);
+    aiOffer.add(scroll);
+    center.add(aiOffer);
+
+    this.add(center);
+
     // Bottom panel
     InfoPanel bottomPanel = new InfoPanel();
     bottomPanel.setLayout(new BorderLayout());
@@ -79,6 +129,18 @@ public class DiplomacyView extends BlackPanel {
     this.add(bottomPanel, BorderLayout.SOUTH);
   }
 
+  /**
+   * Create Tech List from tech
+   * @param techs Which are used for creating Tech List
+   * @return JList full of tech
+   */
+  private JList<Tech> createTechList(final Tech[] techs) {
+    JList<Tech> techList = new JList<>(techs);
+    techList.setCellRenderer(new TechListRenderer());
+    techList.setBackground(Color.BLACK);
+    techList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+    return techList;
+  }
   /**
    * Get human playerinfo
    * @return Human playerinfo
@@ -93,5 +155,12 @@ public class DiplomacyView extends BlackPanel {
    */
   public StarMap getStarMap() {
     return starMap;
+  }
+  /**
+   * Get Diplomatic trade
+   * @return DiplomaticTrade
+   */
+  public DiplomaticTrade getTrade() {
+    return trade;
   }
 }

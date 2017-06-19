b21b99856b26369540d69d14c689abfe34602ad2
Tuomo Untinen <tuomo.untinen@iki.fi>
2017-06-19 21:27:43 +0300

    Updates BattleViewTest class.

--- src/test/java/org/openRealmOfStars/game/States/BattleViewTest.java
+++ src/test/java/org/openRealmOfStars/game/States/BattleViewTest.java
@@ -3,6 +3,8 @@ package org.openRealmOfStars.game.States;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.mockito.Mockito;
 import org.openRealmOfStars.player.combat.Combat;
 import org.openRealmOfStars.player.combat.CombatShip;
@@ -14,7 +16,7 @@ import java.awt.event.ActionListener;
 /**
  *
  * Open Realm of Stars game project
- * Copyright (C) 2016  Tuomo Untinen
+ * Copyright (C) 2016,2017  Tuomo Untinen
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -35,24 +37,44 @@ import java.awt.event.ActionListener;
  */
 public class BattleViewTest {
 
-    private BattleView battleView;
+  /**
+   * BattleView to test
+   */
+  private BattleView battleView;
 
-    @Before
-    public void setUp() {
-        ShipHull shipHull = Mockito.mock(ShipHull.class);
-        Ship ship = Mockito.mock(Ship.class);
-        Mockito.when(ship.getHull()).thenReturn(shipHull);
-        CombatShip combatShip = Mockito.mock(CombatShip.class);
-        Mockito.when(combatShip.getShip()).thenReturn(ship);
-        Combat combat = Mockito.mock(Combat.class);
-        Mockito.when(combat.getCurrentShip()).thenReturn(combatShip);
-        StarMap starMap = Mockito.mock(StarMap.class);
-        Mockito.when(starMap.getMaxX()).thenReturn(10);
-        Mockito.when(starMap.getMaxY()).thenReturn(10);
-        ActionListener actionListener = Mockito.mock(ActionListener.class);
-
-        battleView = new BattleView(combat, starMap, actionListener);
-        assertNotEquals(null, battleView);
-    }
+  /**
+   * Combat to use in battleview test
+   */
+  private Combat combat;
+  /**
+   * StarMap to use in battleview test
+   */
+  private StarMap starMap;
+  /**
+   * ActionListener to use in battleview test
+   */
+  private ActionListener actionListener;
+  
+  @Before
+  public void setUp() {
+      ShipHull shipHull = Mockito.mock(ShipHull.class);
+      Ship ship = Mockito.mock(Ship.class);
+      Mockito.when(ship.getHull()).thenReturn(shipHull);
+      CombatShip combatShip = Mockito.mock(CombatShip.class);
+      Mockito.when(combatShip.getShip()).thenReturn(ship);
+      combat = Mockito.mock(Combat.class);
+      Mockito.when(combat.getCurrentShip()).thenReturn(combatShip);
+      starMap = Mockito.mock(StarMap.class);
+      Mockito.when(starMap.getMaxX()).thenReturn(10);
+      Mockito.when(starMap.getMaxY()).thenReturn(10);
+      actionListener = Mockito.mock(ActionListener.class);
+  }
+  
+  @Test
+  @Category(org.openRealmOfStars.BehaviourTest.class)
+  public void test() {
+      battleView = new BattleView(combat, starMap, actionListener);
+      assertNotEquals(null, battleView);
+  }
 
 }

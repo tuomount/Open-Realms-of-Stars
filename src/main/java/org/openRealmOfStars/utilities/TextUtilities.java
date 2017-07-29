5f0f2c796e2da0a47e99741f6122b588fa455a22
Tuomo Untinen <tuomo.untinen@iki.fi>
2017-07-29 13:35:30 +0300

    Adds TextUtilites to convert integer to ordering number.
    Adds also JUnits for it.

---
+++ src/main/java/org/openRealmOfStars/utilities/TextUtilities.java
@@ -0,0 +1,78 @@
+package org.openRealmOfStars.utilities;
+
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2017  Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* Text Utilities
+*
+*/
+public final class TextUtilities {
+
+  /**
+   * Hiding the constructor
+   */
+  private TextUtilities() {
+    // Nothing to do
+  }
+
+  /**
+   * Get integer as ordering text
+   * @param number Integer to convert ordering text
+   * @return Ordering text
+   * @throws IllegalArgument if integer is below one
+   */
+  public static String getOrderNumberAsText(final int number) {
+    if (number < 1) {
+      throw new IllegalArgumentException("Integers below one are not"
+          + " allowed!");
+    }
+    if (number == 1) {
+      return "first";
+    } else if (number == 2) {
+      return "second";
+    } else if (number == 3) {
+      return "third";
+    } else if (number == 4) {
+      return "fourth";
+    } else if (number == 11) {
+      return "11th";
+    } else if (number == 12) {
+      return "12th";
+    } else if (number == 13) {
+      return "13th";
+    } else {
+      String temp = Integer.toString(number);
+      char lastCh = temp.charAt(temp.length() - 1);
+      char secondLastCh = '0';
+      if (temp.length() > 2) {
+        secondLastCh = temp.charAt(temp.length() - 2);
+      }
+      if (lastCh == '1' && secondLastCh != '1') {
+        return temp + "st";
+      } else if (lastCh == '2' && secondLastCh != '1') {
+        return temp + "nd";
+      } else if (lastCh == '3' && secondLastCh != '1') {
+        return temp + "rd";
+      } else {
+        return temp + "th";
+      }
+    }
+
+  }
+}
---
+++ src/test/java/org/openRealmOfStars/utilities/TextUtilitiestTest.java
@@ -0,0 +1,112 @@
+package org.openRealmOfStars.utilities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class TextUtilitiestTest {
+
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testOne() {
+    String value = TextUtilities.getOrderNumberAsText(1);
+    assertEquals("first", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testTwo() {
+    String value = TextUtilities.getOrderNumberAsText(2);
+    assertEquals("second", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testThree() {
+    String value = TextUtilities.getOrderNumberAsText(3);
+    assertEquals("third", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testFour() {
+    String value = TextUtilities.getOrderNumberAsText(4);
+    assertEquals("fourth", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testFive() {
+    String value = TextUtilities.getOrderNumberAsText(5);
+    assertEquals("5th", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testEleven() {
+    String value = TextUtilities.getOrderNumberAsText(11);
+    assertEquals("11th", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testTwelve() {
+    String value = TextUtilities.getOrderNumberAsText(12);
+    assertEquals("12th", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testThirteen() {
+    String value = TextUtilities.getOrderNumberAsText(13);
+    assertEquals("13th", value);
+  }
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testHundredEleven() {
+    String value = TextUtilities.getOrderNumberAsText(111);
+    assertEquals("111th", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testHundredTwelve() {
+    String value = TextUtilities.getOrderNumberAsText(112);
+    assertEquals("112th", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testHundredThirteen() {
+    String value = TextUtilities.getOrderNumberAsText(113);
+    assertEquals("113th", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testFourteen() {
+    String value = TextUtilities.getOrderNumberAsText(14);
+    assertEquals("14th", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testTwentyOne() {
+    String value = TextUtilities.getOrderNumberAsText(21);
+    assertEquals("21st", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testTwentyTwo() {
+    String value = TextUtilities.getOrderNumberAsText(22);
+    assertEquals("22nd", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testTwentyThree() {
+    String value = TextUtilities.getOrderNumberAsText(23);
+    assertEquals("23rd", value);
+  }
+  @Test
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testTwentyFour() {
+    String value = TextUtilities.getOrderNumberAsText(24);
+    assertEquals("24th", value);
+  }
+  @Test(expected=IllegalArgumentException.class)
+  @Category(org.openRealmOfStars.UnitTest.class)
+  public void testZero() {
+    String value = TextUtilities.getOrderNumberAsText(0);
+    assertEquals("0", value);
+  }
+
+}

e0c99c78baab547eed82ccfb22500e7e80d112f5
Tuomo Untinen <tuomo.untinen@iki.fi>
2017-11-25 22:47:42 +0200

    #136 Fixes universum creation with Scaurians.
    Fixes news image for scaurians.

--- src/main/java/org/openRealmOfStars/player/PlayerInfo.java
+++ src/main/java/org/openRealmOfStars/player/PlayerInfo.java
@@ -330,6 +330,48 @@ public class PlayerInfo {
       addShipStat(stat);
       break;
     }
+    case SCAURIANS: {
+      /*
+       * Scaurians get 1 Combat, 1 Defense, Scout, Colony and Tax center
+       */
+      Tech tech = TechFactory.createRandomTech(TechType.Combat, 1,
+          techList.getListForTypeAndLevel(TechType.Combat, 1));
+      if (tech != null) {
+        techList.addTech(tech);
+      }
+      tech = TechFactory.createRandomTech(TechType.Defense, 1,
+          techList.getListForTypeAndLevel(TechType.Defense, 1));
+      if (tech != null) {
+        techList.addTech(tech);
+      }
+      tech = TechFactory.createHullTech("Colony", 1);
+      if (tech != null) {
+        techList.addTech(tech);
+      }
+      tech = TechFactory.createHullTech("Scout Mk1", 1);
+      if (tech != null) {
+        techList.addTech(tech);
+      }
+      tech = TechFactory.createPropulsionTech("Ion drive Mk1", 1);
+      if (tech != null) {
+        techList.addTech(tech);
+      }
+      tech = TechFactory.createPropulsionTech("Fission source Mk1", 1);
+      if (tech != null) {
+        techList.addTech(tech);
+      }
+      tech = TechFactory.createImprovementTech("Tax center", 1);
+      if (tech != null) {
+        techList.addTech(tech);
+      }
+      ShipDesign design = ShipGenerator.createScout(this);
+      ShipStat stat = new ShipStat(design);
+      addShipStat(stat);
+      design = ShipGenerator.createColony(this, false);
+      stat = new ShipStat(design);
+      addShipStat(stat);
+      break;
+    }
     default:
       ErrorLogger.log("Unexpected race:" + getRace());
     }
--- src/main/java/org/openRealmOfStars/starMap/newsCorp/ImageInstruction.java
+++ src/main/java/org/openRealmOfStars/starMap/newsCorp/ImageInstruction.java
@@ -271,6 +271,7 @@ public class ImageInstruction {
         && !SpaceRace.MOTHOIDS.getNameSingle().equals(image)
         && !SpaceRace.TEUTHIDAES.getNameSingle().equals(image)
         && !SpaceRace.MECHIONS.getNameSingle().equals(image)
+        && !SpaceRace.SCAURIANS.getNameSingle().equals(image)
         && !LOGO.equals(image)) {
       throw new IllegalArgumentException("Illegal image: "
         + image);

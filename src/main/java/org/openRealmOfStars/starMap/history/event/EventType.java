b9d0a51f3238acc48b43662a7051de729a4c5094
Tuomo Untinen <tuomo.untinen@iki.fi>
2018-03-15 06:53:16 +0200

    #158 Stars implementation of history events.

---
+++ src/main/java/org/openRealmOfStars/starMap/history/event/EventType.java
@@ -0,0 +1,48 @@
+package org.openRealmOfStars.starMap.history.event;
+
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2018  Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* Event type for history information
+*
+*/
+public enum EventType {
+  /**
+   * Culture has changed in game map for single sector
+   */
+  CULTURE_CHANGE,
+  /**
+   * Realm colonized planet
+   */
+  PLANET_COLONIZED,
+  /**
+   * Planer conquered
+   */
+  PLANET_CONQUERED,
+  /**
+   * Space combat
+   */
+  SPACE_COMBAT,
+  /**
+   * Diplomatic relation changed between two realms
+   */
+  DIPLOMATIC_RELATION_CHANGE
+
+
+}

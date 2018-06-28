c02f6eb65f0de3dc9b7c3b54cf4588a6927bb6a7
Tuomo Untinen <tuomo.untinen@iki.fi>
2018-06-28 22:10:31 +0300

    Update version to 0.7.0Alpha.

--- src/main/java/org/openRealmOfStars/game/Game.java
+++ src/main/java/org/openRealmOfStars/game/Game.java
@@ -119,7 +119,7 @@ public class Game implements ActionListener {
   /**
    * Game version number
    */
-  public static final String GAME_VERSION = "0.6.42Alpha";
+  public static final String GAME_VERSION = "0.7.0Alpha";
 
   /**
    * Animation timer used for animation

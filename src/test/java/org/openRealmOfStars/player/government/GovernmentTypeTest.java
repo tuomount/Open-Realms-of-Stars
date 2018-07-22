e7415ef49a1a1a65e0ceaf239df11a12ad9a21e7
Tuomo Untinen <tuomo.untinen@iki.fi>
2018-07-23 00:19:13 +0300

    #196 Adds JUnit which can dump out Government types as markdown.

--- src/test/java/org/openRealmOfStars/player/government/GovernmentTypeTest.java
+++ src/test/java/org/openRealmOfStars/player/government/GovernmentTypeTest.java
@@ -33,84 +33,93 @@ public class GovernmentTypeTest {
   @Test
   @Category(org.openRealmOfStars.UnitTest.class)
   public void testDescriptions() {
-    assertEquals("### Democracy\n"
+    String[] expected = new String[GovernmentType.values().length];
+    expected[0] = "### Democracy\n"
         + "* Generic happiness: 1\n"
         + "* Diplomatic bonus: 1\n"
         + "* Trade bonus: 1\n"
         + "* War resistance: -1\n"
         + "* Production bonus: 1\n"
-        + "* Credit rush\n", GovernmentType.DEMOCRACY.getDescription(true));
-    assertEquals("<html>Alliance<br>"
-        + "<li> Generic happiness: 1<br>"
-        + "<li> Diplomatic bonus: 1<br>"
-        + "<li> Trade bonus: 1<br>"
-        + "<li> War resistance: -1<br>"
-        + "<li> Production bonus: 1<br>"
-        + "<li> Credit rush<br></html>", GovernmentType.ALLIANCE.getDescription(false));
-    assertEquals("### Federation\n"
+        + "* Credit rush\n";
+    expected[1] = "### Alliance\n"
         + "* Generic happiness: 1\n"
         + "* Diplomatic bonus: 1\n"
         + "* Trade bonus: 1\n"
-        + "* Credit rush\n", GovernmentType.FEDERATION.getDescription(true));
-    assertEquals("### Republic\n"
+        + "* War resistance: -1\n"
+        + "* Production bonus: 1\n"
+        + "* Credit rush\n";
+    expected[2] = "### Federation\n"
         + "* Generic happiness: 1\n"
         + "* Diplomatic bonus: 1\n"
         + "* Trade bonus: 1\n"
-        + "* Credit rush\n", GovernmentType.REPUBLIC.getDescription(true));
-    assertEquals("### Guild\n"
+        + "* Credit rush\n";
+    expected[3] = "### Republic\n"
+        + "* Generic happiness: 1\n"
+        + "* Diplomatic bonus: 1\n"
+        + "* Trade bonus: 1\n"
+        + "* Credit rush\n";
+    expected[4] = "### Guild\n"
         + "* Trade bonus: 2\n"
         + "* Credit bonus: 1\n"
-        + "* Credit rush\n", GovernmentType.GUILD.getDescription(true));
-    assertEquals("### Enterprise\n"
+        + "* Credit rush\n";
+    expected[5] = "### Enterprise\n"
         + "* Trade bonus: 2\n"
         + "* Credit bonus: 1\n"
-        + "* Credit rush\n", GovernmentType.ENTERPRISE.getDescription(true));
-    assertEquals("### Hegemony\n"
+        + "* Credit rush\n";
+    expected[6] = "### Hegemony\n"
         + "* Generic happiness: -1\n"
         + "* War resistance: 1\n"
-        + "* Research bonus: 1\n", GovernmentType.HEGEMONY.getDescription(true));
-    assertEquals("### Nest\n"
-        + "* No effects on happines nor war fatigue",
-        GovernmentType.NEST.getDescription(true));
-    assertEquals("### Hive-mind\n"
-        + "* No effects on happines nor war fatigue",
-        GovernmentType.HIVEMIND.getDescription(true));
-    assertEquals("### AI\n"
-        + "* No effects on happines nor war fatigue",
-        GovernmentType.AI.getDescription(true));
-    assertEquals("### Empire\n"
+        + "* Research bonus: 1\n";
+    expected[7] = "### Nest\n"
+        + "* No effects on happines nor war fatigue";
+    expected[8] = "### Hive-mind\n"
+        + "* No effects on happines nor war fatigue";
+    expected[9] = "### AI\n"
+        + "* No effects on happines nor war fatigue";
+    expected[10] = "### Empire\n"
         + "* Generic happiness: -1\n"
         + "* War resistance: 1\n"
         + "* Production bonus: 1\n"
-        + "* Population rush\n", GovernmentType.EMPIRE.getDescription(true));
-    assertEquals("### Kingdom\n"
+        + "* Population rush\n";
+    expected[11] = "### Kingdom\n"
         + "* Generic happiness: -1\n"
         + "* War resistance: 1\n"
         + "* Production bonus: 1\n"
-        + "* Population rush\n", GovernmentType.KINGDOM.getDescription(true));
-    assertEquals("### Hierarchy\n"
+        + "* Population rush\n";
+    expected[12] = "### Hierarchy\n"
         + "* Generic happiness: -1\n"
         + "* War resistance: 1\n"
         + "* Production bonus: 1\n"
-        + "* Population rush\n", GovernmentType.HIERARCHY.getDescription(true));
-    assertEquals("### Horde\n"
+        + "* Population rush\n";
+    expected[13] = "### Horde\n"
         + "* Generic happiness: -1\n"
         + "* War resistance: 1\n"
         + "* Food bonus: 1\n"
         + "* War happiness\n"
-        + "* Population rush\n", GovernmentType.HORDE.getDescription(true));
-    assertEquals("### Horde\n"
+        + "* Population rush\n";
+    expected[14] = "### Horde\n"
         + "* Generic happiness: -1\n"
         + "* War resistance: 1\n"
         + "* Production bonus: 1\n"
         + "* War happiness\n"
-        + "* Population rush\n", GovernmentType.MECHANICAL_HORDE.getDescription(true));
-    assertEquals("### Clan\n"
+        + "* Population rush\n";
+    expected[15] = "### Clan\n"
         + "* Generic happiness: -1\n"
         + "* War resistance: 1\n"
         + "* Food bonus: 1\n"
         + "* War happiness\n"
-        + "* Population rush\n", GovernmentType.CLAN.getDescription(true));
+        + "* Population rush\n";
+    for (int i = 0; i <  GovernmentType.values().length; i++) {
+      GovernmentType government = GovernmentType.values()[i];
+      assertEquals(expected[i], government.getDescription(true));
+      if (i == 0) {
+        String result = government.getDescription(false);
+        if (!result.startsWith("<html>")) {
+          assertFalse(true);
+        }
+      }
+      //System.out.println(government.getDescription(true));
+    }
   }
 
   @Test

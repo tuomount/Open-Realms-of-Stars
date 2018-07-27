ff4c62a4041dca952284817314c2b32f4e6c459b
Tuomo Untinen <tuomo.untinen@iki.fi>
2018-07-27 13:18:31 +0300

    #199 Adds top panel to fleet trade view.

--- src/main/java/org/openRealmOfStars/game/States/FleetTradeView.java
+++ src/main/java/org/openRealmOfStars/game/States/FleetTradeView.java
@@ -1,14 +1,22 @@
 package org.openRealmOfStars.game.States;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 
 import org.openRealmOfStars.game.GameCommands;
 import org.openRealmOfStars.gui.buttons.SpaceButton;
+import org.openRealmOfStars.gui.icons.Icons;
 import org.openRealmOfStars.gui.infopanel.InfoPanel;
+import org.openRealmOfStars.gui.labels.IconLabel;
+import org.openRealmOfStars.gui.labels.SpaceLabel;
 import org.openRealmOfStars.gui.panels.BigImagePanel;
 import org.openRealmOfStars.gui.panels.BlackPanel;
+import org.openRealmOfStars.gui.panels.SpaceGreyPanel;
 import org.openRealmOfStars.player.PlayerInfo;
 import org.openRealmOfStars.player.fleet.Fleet;
 import org.openRealmOfStars.starMap.StarMap;
@@ -68,6 +76,21 @@ public class FleetTradeView extends BlackPanel {
   private StarMap starMap;
 
   /**
+   * Planet owner's empire name if fleet is orbiting planet.
+   */
+  private SpaceLabel ownerLabel;
+
+  /**
+   * Icon label showing for total amount of people on planet where
+   * fleet is orbiting.
+   */
+  private IconLabel totalPeople;
+  /**
+   * Amount of metal on planet where fleet is orbiting.
+   */
+  private IconLabel metal;
+
+  /**
    * Fleet Trade view constructor
    * @param map StarMap
    * @param info PlayerInfo who is about to trade
@@ -82,6 +105,48 @@ public class FleetTradeView extends BlackPanel {
     this.planet = planet;
     this.info = info;
     this.fleet = fleet;
+
+    // Top Panel
+    InfoPanel topPanel = null;
+    if (this.planet != null) {
+      topPanel = new InfoPanel();
+      topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+
+      topPanel.add(Box.createRigidArea(new Dimension(15, 25)));
+      SpaceGreyPanel panel = new SpaceGreyPanel();
+      panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+      if (planet.getPlanetPlayerInfo() != null) {
+        ownerLabel = new SpaceLabel(planet.getPlanetPlayerInfo()
+            .getEmpireName());
+        panel.add(ownerLabel);
+      }
+      totalPeople = new IconLabel(null,
+          Icons.getIconByName(Icons.ICON_PEOPLE),
+          ": " + planet.getTotalPopulation());
+      totalPeople.setToolTipText("Total number of people on planet.");
+      totalPeople.setAlignmentX(Component.LEFT_ALIGNMENT);
+      panel.add(totalPeople);
+      topPanel.add(panel);
+      topPanel.add(Box.createRigidArea(new Dimension(25, 25)));
+
+      panel = new SpaceGreyPanel();
+      panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+      metal = new IconLabel(null, Icons.getIconByName(Icons.ICON_METAL),
+          ": " + planet.getMetal());
+      metal.setToolTipText("Total metal on surface");
+      metal.setAlignmentX(Component.LEFT_ALIGNMENT);
+      panel.add(metal);
+      topPanel.add(panel);
+      topPanel.add(Box.createRigidArea(new Dimension(25, 25)));
+      topPanel.setTitle(planet.getName());
+
+      panel = new SpaceGreyPanel();
+      panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+      topPanel.add(panel);
+      topPanel.add(Box.createRigidArea(new Dimension(50, 25)));
+      topPanel.setTitle(planet.getName());
+    }
 
     // Background image
     imgBase = new BigImagePanel(planet, true, null);

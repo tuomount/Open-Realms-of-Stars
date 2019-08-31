33e6b680ae0093a44308d53a39b63d46e609fbc4
Tuomo Untinen <tuomo.untinen@iki.fi>
2019-08-31 09:56:30 +0300

    #258 Adds random event types.
    This commit just adds enums which are starting point for
    different random events.

---
+++ src/main/java/org/openRealmOfStars/starMap/randomEvent/BadRandomType.java
@@ -0,0 +1,69 @@
+package org.openRealmOfStars.starMap.randomEvent;
+
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2019 Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* Bad Random event type
+*
+*/
+public enum BadRandomType {
+  /**
+   * Meteor crashes into planet and can kill populations or destroy buildings.
+   * Defensive turret can shoot it down and then provide more metal to planet.
+   */
+  METEOR_HIT,
+  /**
+   * This event will kill all population expect one. One population is
+   * resistant for virus. This has no effect if race is Mechion.
+   */
+  DEADLY_VIRUS_OUTBREAK,
+  /**
+   * Fleet in deep space turns into space pirates.
+   */
+  MUTINY,
+  /**
+   * Space pirates appear near the planet and are ready for raiding.
+   */
+  RAIDERS,
+  /**
+   * Solar activity increases and all planets in same system increase one
+   *  radiation level.
+   */
+  SOLAR_ACTIVITY_INCREASE,
+  /**
+   * One planet has aggressive wild life with power 12 and planet
+   * must fight against it.
+   */
+  AGGRESSIVE_WILD_LIFE,
+  /**
+   * Planet climate changes to arid. If it is already arid then it turns
+   * to desert.
+   */
+  CLIMATE_CHANGE,
+  /**
+   * Massive data lost in research labs. One tech research points for current
+   * level is set to zero.
+   */
+  MASSIVE_DATA_LOST,
+  /**
+   * Massive corruption scandal found in government.
+   * Half of the credits are gone.
+   */
+  CORRUPTION_SCANDAL;
+}
---
+++ src/main/java/org/openRealmOfStars/starMap/randomEvent/GoodRandomType.java
@@ -0,0 +1,48 @@
+package org.openRealmOfStars.starMap.randomEvent;
+
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2019 Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* Good Random event type
+*
+*/
+public enum GoodRandomType {
+  /**
+   * Mysterious signal received from solar system.
+   * This signal is from another realm which player hasn't met yet.
+   */
+  MYSTERIOUS_SIGNAL,
+  /**
+   * Climate changes on planet to provide more food.
+   * DESERT->ARID->None->Lush Vegatation->Paradise
+   */
+  CLIMATE_CHANGE,
+  /**
+   * Double the research points in one tech level
+   */
+  TECHNICAL_BREAKTHROUGH,
+  /**
+   * Lost treasure found, +50 credits.
+   */
+  LOST_TREASURE_FOUND,
+  /**
+   * All planets in system decrease radiation level.
+   */
+  SOLAR_ACTIVITY_DIMISHED
+}

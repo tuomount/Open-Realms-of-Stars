30e0736c65c45a4a7f47a51f30e204c96c5c5931
Tuomo Untinen <tuomo.untinen@iki.fi>
2019-10-19 12:36:37 +0300

    Update version to 0.11.51Beta.
    Fixes possible bug where planet population drops to zero.

--- src/main/java/org/openRealmOfStars/game/Game.java
+++ src/main/java/org/openRealmOfStars/game/Game.java
@@ -135,7 +135,7 @@ public class Game implements ActionListener {
   /**
    * Game version number
    */
-  public static final String GAME_VERSION = "0.11.50Beta";
+  public static final String GAME_VERSION = "0.11.51Beta";
 
   /**
    * Animation timer used for animation

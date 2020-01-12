f5fd7f98dfe3866e5ccf81f42a5fbd90c3ee761d
Tuomo Untinen <tuomo.untinen@iki.fi>
2020-01-12 23:02:38 +0200

    Update version to 0.13.5Beta.

--- src/main/java/org/openRealmOfStars/game/Game.java
+++ src/main/java/org/openRealmOfStars/game/Game.java
@@ -143,7 +143,7 @@ public class Game implements ActionListener {
   /**
    * Game version number
    */
-  public static final String GAME_VERSION = "0.13.4Beta";
+  public static final String GAME_VERSION = "0.13.5Beta";
 
   /**
    * Animation timer used for animation

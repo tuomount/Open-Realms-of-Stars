3f8fcacd4ba9eb34a7d56093c13e7e2f18c869c0
Tuomo Untinen <tuomo.untinen@iki.fi>
2021-04-01 23:57:28 +0300

    #420 Starts implementation of leader stats.

---
+++ src/main/java/org/openRealmOfStars/player/leader/stats/LeaderStats.java
@@ -0,0 +1,148 @@
+package org.openRealmOfStars.player.leader.stats;
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2021 Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* Leader Stats
+*
+*/
+public class LeaderStats {
+  /**
+   * Ruler reign length in turns.
+   */
+  private int reignLength;
+  /**
+   * How many times leader has been ruler.
+   */
+  private int numberOfRulers;
+  /**
+   * How many battles leader have had.
+   */
+  private int numberOfBattles;
+  /**
+   * How many espionage mission leader have had.
+   */
+  private int numberOfEspionage;
+  /**
+   * How many times leader has explored space anomaly.
+   */
+  private int numberOfAnomaly;
+  /**
+   * Total number of turn leader has been commander.
+   */
+  private int commanderLength;
+  /**
+   * Total number of turn leader has been governor.
+   */
+  private int governorLength;
+  /**
+   * Total number of jail times for leader.
+   */
+  private int numberOfJailTime;
+  /**
+   * Total number of turns in jail.
+   */
+  private int jailTime;
+  /**
+   * Total number of killed another leaders.
+   */
+  private int killedAnotherLeader;
+  /**
+   * How many battles leader have had against pirates/privateers.
+   */
+  private int numberOfPirateBattles;
+  /**
+   * How many privateer leader has done.
+   */
+  private int numberOfPrivateers;
+  /**
+   * How many trades leader have had.
+   */
+  private int numberOfTrades;
+
+  /**
+   * Constructor of leader stats.
+   */
+  public LeaderStats() {
+    reignLength = 0;
+    numberOfRulers = 0;
+    numberOfBattles = 0;
+    numberOfEspionage = 0;
+    numberOfAnomaly = 0;
+    commanderLength = 0;
+    governorLength = 0;
+    numberOfJailTime = 0;
+    jailTime = 0;
+    killedAnotherLeader = 0;
+    numberOfPirateBattles = 0;
+    numberOfPrivateers = 0;
+    numberOfTrades = 0;
+  }
+
+  /**
+   * Set Stat value.
+   * @param type Stat type which to set.
+   * @param value Value where to set.
+   */
+  public void setStat(final StatType type, final int value) {
+    if (value >= 0 && value <= 65535) {
+      switch (type) {
+        case RULER_REIGN_LENGTH: reignLength = value; break;
+        case NUMBER_OF_RULER: numberOfRulers = value; break;
+        case NUMBER_OF_BATTLES: numberOfBattles = value; break;
+        case NUMBER_OF_ESPIONAGE: numberOfEspionage = value; break;
+        case NUMBER_OF_ANOMALY: numberOfAnomaly = value; break;
+        case COMMANDER_LENGTH: commanderLength = value; break;
+        case GOVERNOR_LENGTH: governorLength = value; break;
+        case NUMBER_OF_JAIL_TIME: numberOfJailTime = value; break;
+        case JAIL_TIME: jailTime = value; break;
+        case KILLED_ANOTHER_LEADER: killedAnotherLeader = value; break;
+        case NUMBER_OF_PIRATE_BATTLES: numberOfPirateBattles = value; break;
+        case NUMBER_OF_PRIVATEERING: numberOfPrivateers = value; break;
+        case NUMBER_OF_TRADES: numberOfTrades = value; break;
+        default: throw new IllegalArgumentException("Unexpected stat type: "
+            + type.toString());
+      }
+    }
+  }
+
+  /**
+   * Get leader stat for certain stat type.
+   * @param type Stat type for get.
+   * @return Stat value.
+   */
+  public int getStat(final StatType type) {
+    switch (type) {
+      case RULER_REIGN_LENGTH: return reignLength;
+      case NUMBER_OF_RULER: return numberOfRulers;
+      case NUMBER_OF_BATTLES: return numberOfBattles;
+      case NUMBER_OF_ESPIONAGE: return numberOfEspionage;
+      case NUMBER_OF_ANOMALY: return numberOfAnomaly;
+      case COMMANDER_LENGTH: return commanderLength;
+      case GOVERNOR_LENGTH: return governorLength;
+      case NUMBER_OF_JAIL_TIME: return numberOfJailTime;
+      case JAIL_TIME: return jailTime;
+      case KILLED_ANOTHER_LEADER: return killedAnotherLeader;
+      case NUMBER_OF_PIRATE_BATTLES: return numberOfPirateBattles;
+      case NUMBER_OF_PRIVATEERING: return numberOfPrivateers;
+      case NUMBER_OF_TRADES: return numberOfTrades;
+      default: throw new IllegalArgumentException("Unexpected stat type: "
+          + type.toString());
+    }
+  }
+}
---
+++ src/main/java/org/openRealmOfStars/player/leader/stats/StatType.java
@@ -0,0 +1,104 @@
+package org.openRealmOfStars.player.leader.stats;
+
+/**
+*
+* Open Realm of Stars game project
+* Copyright (C) 2021 Tuomo Untinen
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see http://www.gnu.org/licenses/
+*
+*
+* Leader Stat type
+*
+*/
+public enum StatType {
+
+  /**
+   * Total amount of turns as ruler.
+   */
+  RULER_REIGN_LENGTH,
+  /**
+   * How many times has been selected for ruler.
+   */
+  NUMBER_OF_RULER,
+  /**
+   * How many times has been in space battles.
+   */
+  NUMBER_OF_BATTLES,
+  /**
+   * How many times has done espionage missions.
+   */
+  NUMBER_OF_ESPIONAGE,
+  /**
+   * How many space anomalies have been studied.
+   */
+  NUMBER_OF_ANOMALY,
+  /**
+   * How many turns have been as commander.
+   */
+  COMMANDER_LENGTH,
+  /**
+   * How many turns have been as governor.
+   */
+  GOVERNOR_LENGTH,
+  /**
+   * How many times have been in jail.
+   */
+  NUMBER_OF_JAIL_TIME,
+  /**
+   * How many turns have been in jail.
+   */
+  JAIL_TIME,
+  /**
+   * How many times have killed another leader.
+   */
+  KILLED_ANOTHER_LEADER,
+  /**
+   * How many times has been in space battle with pirates/privateers.
+   */
+  NUMBER_OF_PIRATE_BATTLES,
+  /**
+   * How many times has done privateering.
+   */
+  NUMBER_OF_PRIVATEERING,
+  /**
+   * How many times has done trading.
+   */
+  NUMBER_OF_TRADES;
+
+  /**
+   * Get Stat Type as byte.
+   * @return Byte value for stat type.
+   */
+  public byte getAsByte() {
+    switch (this) {
+      case RULER_REIGN_LENGTH: return 0;
+      case NUMBER_OF_RULER: return 1;
+      case NUMBER_OF_BATTLES: return 2;
+      case NUMBER_OF_ESPIONAGE: return 3;
+      case NUMBER_OF_ANOMALY: return 4;
+      case COMMANDER_LENGTH: return 5;
+      case GOVERNOR_LENGTH: return 6;
+      case NUMBER_OF_JAIL_TIME: return 7;
+      case JAIL_TIME: return 8;
+      case KILLED_ANOTHER_LEADER: return 9;
+      case NUMBER_OF_PIRATE_BATTLES: return 10;
+      case NUMBER_OF_PRIVATEERING: return 11;
+      case NUMBER_OF_TRADES: return 12;
+      default:
+        throw new IllegalArgumentException("Unexpected StatType:"
+            + this.toString());
+    }
+  }
+}

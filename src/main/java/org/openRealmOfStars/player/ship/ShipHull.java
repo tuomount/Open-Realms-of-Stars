8d42ce91c8eca8ffcf949d5833fdf93743491266
BottledByte <62666666+BottledByte@users.noreply.github.com>
2023-11-20 22:14:12 +0100

    #644 Fix minor issue with license header

--- src/main/java/org/openRealmOfStars/player/ship/ShipHull.java
+++ src/main/java/org/openRealmOfStars/player/ship/ShipHull.java
@@ -1,7 +1,7 @@
 package org.openRealmOfStars.player.ship;
 /*
  * Open Realm of Stars game project
- * Copyright (C) 2016-2020 Tuomo Untinen
+ * Copyright (C) 2016-2021 Tuomo Untinen
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License

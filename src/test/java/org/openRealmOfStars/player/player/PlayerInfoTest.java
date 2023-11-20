b484dfd2e75daa7b920538848260f7087e17e12d
BottledByte <62666666+BottledByte@users.noreply.github.com>
2023-11-20 20:47:26 +0100

    #644 Add license header to 1 file and fix another one

--- src/test/java/org/openRealmOfStars/player/player/PlayerInfoTest.java
+++ src/test/java/org/openRealmOfStars/player/player/PlayerInfoTest.java
@@ -1,4 +1,22 @@
 package org.openRealmOfStars.player.player;
+/*
+ * Open Realm of Stars game project
+ * Copyright (C) 2017 GodBeom
+ * Copyright (C) 2017-2019 Tuomo Untinen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses/
+ */
 
 import static org.junit.Assert.*;
 
@@ -24,25 +42,6 @@ import org.openRealmOfStars.starMap.randomEvent.RandomEvent;
 import org.mockito.Mockito;
 
 /**
- * 
- * Open Realm of Stars game project 
- * Copyright (C) 2017 GodBeom
- * Copyright (C) 2017-2019 Tuomo Untinen
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, see http://www.gnu.org/licenses/
- * 
- * 
  * Test for PlayerInfo class
  */
 
@@ -556,7 +555,7 @@ public class PlayerInfoTest {
       assertNull(info.getRandomEventOccured());
     }
 
-    
+
     /**
      * input : sun, fleet output : unchartedSector percent purpose : test
      * getUnchatedValueSystem method
@@ -673,7 +672,7 @@ public class PlayerInfoTest {
         stateResult = player.getSectorVisibility(coord);
         assertEquals(FOG_OF_WAR, stateResult);
     }
-    
+
     /**
      */
     @Test

b856452544d41b554ae1b1d7c104266bd5a124ed
BottledByte <62666666+BottledByte@users.noreply.github.com>
2023-11-20 15:48:13 +0100

    #644 Fix license header in 26 files

--- src/test/java/org/openRealmOfStars/game/States/PlayerSetupViewTest.java
+++ src/test/java/org/openRealmOfStars/game/States/PlayerSetupViewTest.java
@@ -1,4 +1,21 @@
 package org.openRealmOfStars.game.States;
+/*
+ * Open Realm of Stars game project
+ * Copyright (C) 2017 Tuomo Untinen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses/
+ */
 
 import static org.junit.Assert.*;
 
@@ -13,24 +30,8 @@ import org.openRealmOfStars.player.AiDifficulty;
 import org.openRealmOfStars.player.SpaceRace.SpaceRace;
 import org.openRealmOfStars.player.government.GovernmentType;
 import org.openRealmOfStars.starMap.GalaxyConfig;
+
 /**
-*
-* Open Realm of Stars game project
-* Copyright (C) 2017 Tuomo Untinen
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, see http://www.gnu.org/licenses/
-*
 *
 * Test for PlayerSetupView class
 *

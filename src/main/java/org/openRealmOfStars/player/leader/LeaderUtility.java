37e1ed5c9de4479db22b4173cb654b30bc240978
BottledByte <62666666+BottledByte@users.noreply.github.com>
2023-11-24 17:10:22 +0100

    #642 Add missing copyright line to LeaderUtility

--- src/main/java/org/openRealmOfStars/player/leader/LeaderUtility.java
+++ src/main/java/org/openRealmOfStars/player/leader/LeaderUtility.java
@@ -2,6 +2,7 @@ package org.openRealmOfStars.player.leader;
 /*
  * Open Realm of Stars game project
  * Copyright (C) 2020-2023 Tuomo Untinen
+ * Copyright (C) 2023 BottledByte
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License

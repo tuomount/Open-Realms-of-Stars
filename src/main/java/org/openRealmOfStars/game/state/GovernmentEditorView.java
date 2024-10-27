4f596b31e547f6bc29d0cfec87e12afce528850d
Tuomo Untinen <tuomo.untinen@iki.fi>
2024-10-27 22:34:11 +0200

    #743 Government editor continuing...

--- src/main/java/org/openRealmOfStars/game/state/GovernmentEditorView.java
+++ src/main/java/org/openRealmOfStars/game/state/GovernmentEditorView.java
@@ -18,14 +18,21 @@ package org.openRealmOfStars.game.state;
  */
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JTextField;
 
 import org.openRealmOfStars.game.GameCommands;
 import org.openRealmOfStars.gui.buttons.SpaceButton;
 import org.openRealmOfStars.gui.infopanel.InfoPanel;
+import org.openRealmOfStars.gui.labels.SpaceLabel;
 import org.openRealmOfStars.gui.panels.BlackPanel;
+import org.openRealmOfStars.gui.util.GuiFonts;
+import org.openRealmOfStars.gui.util.GuiStatics;
 
 /**
  * Editor for government JSON files with UI.
@@ -38,6 +45,10 @@ public class GovernmentEditorView extends BlackPanel {
   */
   private static final long serialVersionUID = 1L;
 
+  /**
+   * Government name.
+   */
+  private JTextField governmentNameField;
   /**
    * Constructor for GovernmentEditorView.
    *
@@ -52,6 +63,17 @@ public class GovernmentEditorView extends BlackPanel {
     governmentInfoPanel.setTitle("New government");
     governmentInfoPanel.setLayout(new BoxLayout(governmentInfoPanel,
         BoxLayout.Y_AXIS));
+    SpaceLabel label = new SpaceLabel("Government name:");
+    label.setAlignmentX(Component.LEFT_ALIGNMENT);
+    governmentInfoPanel.add(label);
+    governmentInfoPanel.add(Box.createRigidArea(new Dimension(10, 5)));
+    governmentNameField = new JTextField("Custom government");
+    governmentNameField.setBackground(GuiStatics.getDeepSpaceDarkColor());
+    governmentNameField.setForeground(GuiStatics.getCoolSpaceColor());
+    governmentNameField.setFont(GuiFonts.getFontCubellanSmaller());
+    governmentNameField.setMaximumSize(new Dimension(Integer.MAX_VALUE,
+        GuiStatics.TEXT_FIELD_HEIGHT));
+    governmentInfoPanel.add(governmentNameField);
     mainPanel.add(governmentInfoPanel);
     // Bottom panel
     InfoPanel bottomPanel = new InfoPanel();
